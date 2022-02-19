a188f9a706af809d4e982a6a5ca7a9f527096ca1
moni <aanilsaint26@gmail.com>
2022-02-19 12:37:28 +0000

    idk anymore ;-;

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="*" element={<App />} />
+        <Route path="/*" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>,
