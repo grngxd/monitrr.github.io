04d543d949625435a993137fc9f96fc9d4cfe328
moni <aanilsaint26@gmail.com>
2022-02-19 12:21:59 +0000

    404 Page w/ React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Content from './components/Content';
 import Projects from './components/Projects';
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
+
 import App from './App';
+import NotFound from './NotFound';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
