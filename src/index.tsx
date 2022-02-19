fe669988f2536d6b193e27f6e88b6c4f82b4080e
moni <aanilsaint26@gmail.com>
2022-02-19 12:33:22 +0000

    ;-;-;-;

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
-
 import App from './App';
-import NotFound from './NotFound';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path='*' element={<NotFound/>} />
+        <Route path="*" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>,
