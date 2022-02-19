3738153e9a5748bc97ff0a387296c285ee71ba84
moni <aanilsaint26@gmail.com>
2022-02-19 11:33:56 +0000

    Fixed tags again

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -4,21 +4,19 @@ import {Helmet} from "react-helmet";
 function Tags() {
   return (
     <Helmet>
-            <title>monitrr</title>
-            <meta name="title" content="monitrr"/>
-            <meta name="description" content="heyyo! :)"/>
+        <title>monitrr</title>
+        <meta name="description" content="heyyo!"/>
 
-            <meta property="og:type" content="website"/>
-            <meta property="og:url" content="https://metatags.io/"/>
-            <meta property="og:title" content="monitrr"/>
-            <meta property="og:description" content="heyyo! :)"/>
-            <meta property="og:image" content={"/projects/Portfolio.png"}/>
+        <meta property="og:url" content="https://moni.is-a.dev"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="monitrr"/>
+        <meta property="og:description" content="heyyo!"/>
+        <meta property="og:image" content="https://moni.is-a.dev/projects/Portfolio.png"/>
 
-            <meta property="twitter:card" content="summary_large_image"/>
-            <meta property="twitter:url" content="https://metatags.io/"/>
-            <meta property="twitter:title" content="monitrr"/>
-            <meta property="twitter:description" content="heyyo! :)"/>
-            <meta property="twitter:image" content={"/projects/Portfolio.png"}/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content="monitrr"/>
+        <meta name="twitter:description" content="heyyo!"/>
+        <meta name="twitter:image" content="https://moni.is-a.dev/projects/Portfolio.png"/>
     </Helmet>
   )
 }
