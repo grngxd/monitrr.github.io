a31aca37f7e0a7a38dd85a81d9bfddc852421412
moni <aanilsaint26@gmail.com>
2022-02-19 11:38:08 +0000

    ;-;

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import Navbar from './components/Navbar';
 import Content from './components/Content';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
-import Tags from './components/Tags';
 
 function App() {
   return (
     <div className="app">
-      <Tags/>
       <Navbar/>
       <Content/>
       <Projects/>
