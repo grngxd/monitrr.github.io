20b7e2b4448e9a1be54d8152e6551cd9a36f0976
moni <aanilsaint26@gmail.com>
2022-02-19 12:30:31 +0000

    ;-;-;

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
 
 import App from './App';
