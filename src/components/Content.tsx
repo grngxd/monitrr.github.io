c7cb1288db10d41715bfdb0ad94d90a472021797
moni <aanilsaint26@gmail.com>
2022-03-18 20:00:13 +0000

    hi

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -9,8 +9,7 @@ function Content() {
         <h1 className='font-bold text-2xl moni-gradient'>
           monitrr
         </h1>
-        {/* <p className='font-light'>front end developer, game developer, amateur pianist</p> */}
-        <Prescence/>
+        <p className='font-light'>front end developer, game developer, amateur pianist</p>
         
           <div className='md:flex flex-col hidden items-center mt-5'>
               <img src={"/programmer.svg"} alt="Programmer" className="w-56 flex h-100 drop-shadow-sm scale-100 hover:scale-125 hover:drop-shadow-lg hover:mt-5 hover:mb-5 transition-all duration-300"/>
@@ -21,36 +20,6 @@ function Content() {
         </div>
     </div>
   )
-}
-
-function Prescence(){
-  const lanyard = useLanyard({
-		userId: "829372486780715018",
-	});
-
-  if(lanyard.isValidating){
-    return <div className='w-full flex items-center justify-center'><CircularProgress className='text-monitrr-200 hover:text-monitrr-100 transition-all duration-300' color='inherit' variant='indeterminate'/></div>;
-  }else{
-    if(typeof lanyard.data?.data.activities !== 'undefined'){
-    for(var i in lanyard.data?.data.activities){
-      if(lanyard.data?.data.activities[i].type === 4){
-        return (<div>
-          <p className='font-light'>front end developer, game developer, amateur pianist</p>
-        <p className='font-light text-neutral-600 text-sm'>{lanyard.data?.data.activities[i].emoji?.name} {lanyard.data?.data.activities[i].state}</p>
-        </div>)
-      }else{
-        return (<div>
-          <p className='font-light'>front end developer, game developer, amateur pianist</p>
-        <p className='font-light text-neutral-600 text-sm'><span className='font-bold text-sm'>PLAYING: </span>{lanyard.data?.data.activities[i].name}</p>
-        </div>)
-      }
-    }
-    }else{
-      return <p className='font-light'>front end developer, game developer, amateur pianist</p>
-    }
-  }
-
-  return <p className='font-light'>front end developer, game developer, amateur pianist</p>;
 }
 
 export default Content
