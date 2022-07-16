765eb668775da4216fc116bce50ae91f0bc6c391
moni <36968271+monitrr@users.noreply.github.com>
2022-07-17 00:39:39 +0100

    s

--- pages/components/Button.tsx
+++ pages/components/Button.tsx
@@ -2,7 +2,7 @@ export default function Button(props : any) {
     return (
         <a href={props.href}>
             <div className="text-black text-xl hover:bg-[#4501ad] p-2 rounded-md transition-all duration-300 hover:text-white !bg-opacity-70">
-                    <props.Icon />
+                    <props.icon />
             </div>
         </a>
     )
--- pages/components/Buttons.tsx
+++ pages/components/Buttons.tsx
@@ -5,9 +5,9 @@ import Button from './Button';
 export default function Buttons() {
     return (
         <div className="flex gap-2 max-h-28 min-h-28 h-28 items-center">
-                <Button Icon={FiGithub} href="https://github.com/monitrr"/>
-                <Button Icon={FiCreditCard} href="https://ko-fi.com/monitrr"/>
-                <Button Icon={FiAtSign} href="mailto:monitrrlmao@gmail.com"/>
+                <Button icon={FiGithub} href="https://github.com/monitrr"/>
+                <Button icon={FiCreditCard} href="https://ko-fi.com/monitrr"/>
+                <Button icon={FiAtSign} href="mailto:monitrrlmao@gmail.com"/>
         </div>
     )
 }
